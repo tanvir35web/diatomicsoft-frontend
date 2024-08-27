b36197f6dc29e30dc065e694f45b091c0b9a86bc
tanvir35web <islammdtanvirul@gmail.com>
2024-08-28 00:57:28 +0600

    hero mobile resp[onsive

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -1,16 +1,19 @@
 const HeroSection = () => {
   return (
-    <>
-      <div>
-        <div className="flex flex-col gap-4 bg-slate-900 items-center justify-center w-full h-[50vh]">
-          <div className="flex flex-col gap-4 items-center justify-center" >
-          <h1 className="text-6xl font-bold text-white"><span className="text-yellow-500">Empowering</span> Your <span className="text-blue-600">Digital Transformation</span></h1>
-          <p className="text-xl text-white">We are a team of passionate developers, designers, and engineers who create innovative solutions.ringing Your Vision to Life with Advanced Technology</p>
-          </div>
-        </div>
+    <div className="flex flex-col gap-4 bg-slate-900 items-center justify-center w-full h-[50vh] px-4 md:px-8 lg:px-16">
+      <div className="flex flex-col gap-4 items-center justify-center text-center">
+        <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold text-white">
+          <span className="text-yellow-500">Empowering</span> Your{" "}
+          <span className="text-blue-600">Digital Transformation</span>
+        </h1>
+        <p className="text-lg md:text-xl lg:text-2xl text-white">
+          We are a team of passionate developers, designers, and engineers who
+          create innovative solutions. Bringing Your Vision to Life with Advanced
+          Technology
+        </p>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
 export default HeroSection;
