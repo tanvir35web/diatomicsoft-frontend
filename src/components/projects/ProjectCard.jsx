ab06f27194c852c488b1ebdc44411fa1e75b53f5
tanvir35web <islammdtanvirul@gmail.com>
2024-09-10 23:24:11 +0600

    project card design uopdate

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
 
-const ProjectCard = ({ coverImage, id, title, description, status, usedTechnology, targetedPlatform }) => {    
+const ProjectCard = ({ coverImage, id, title, description, status, usedTechnology, targetedPlatform }) => {
 
     return (
         <>
-            <div key={id} className="bg-gray-900 cursor-pointer duration-200 border border-gray-700 hover:border-blue-900 p-3 rounded-lg w-[600px] mt-8" >
-                {coverImage && <Image src={coverImage} height={600} width={800} alt={title + " image"} className="w-full h-80 object-cover rounded-t-md" />}
+            <div key={id} className="bg-gray-900 cursor-pointer duration-200 border border-gray-700 hover:border-blue-900 p-3 
+            rounded-lg !w-[350px] " >
+                {coverImage ? (<Image src={coverImage} height={120} width={300} alt={title + " image"} className="w-full h-80 object-cover rounded-t-md" />) : (<div className="h-[120px] w-[300px] rounded-md bg-gray-700"></div>)}
                 <div className="p-4">
-                    <h1 className="text-2xl font-bold">{title}</h1>
-                    <p className="mt-2 text-gray-400">{description}</p>
-                    <p className="mt-4 text-gray-400"><strong>Status:</strong> {status}</p>
-                    <p className="mt-4 text-gray-400"><strong>Technologies:</strong> {usedTechnology}</p>
-                    <p className="mt-4 text-gray-400"><strong>Platforms:</strong> {targetedPlatform}</p>
+                    <h1 className="text-2xl font-bold"> {title} </h1>
+                    <p className="mt-2 text-gray-400"> {description.slice(1, 200)} </p>
+                    <p className="mt-4 text-gray-400"><strong>Status:</strong> {status} </p>
+                    <p className="mt-4 text-gray-400"><strong>Technologies:</strong> {usedTechnology} </p>
+                    <p className="mt-4 text-gray-400"><strong>Platforms:</strong> {targetedPlatform} </p>
                 </div>
             </div>
         </>
--- src/components/projects/latestProjectSection.jsx
+++ src/components/projects/latestProjectSection.jsx
@@ -5,18 +5,17 @@ const LatestProjectSection = () => {
 
   const { projects, status, error } = useFetchProjects();
 
-  console.log(projects);
-
-
 
   return (
     <>
       <div>
         <h1 className="text-6xl font-bold text-grbg-gray-300 p-5 text-center">Our Recent Projects</h1>
         <p className="text-center text-gray-400 max-w-[800px] mx-auto mb-6">Explore our innovative solutions crafted with cutting-edge technologies. From dynamic web applications to modern software systems, each project showcases our commitment to excellence and innovation.</p>
+        
         {status === 'loading' && <p>Loading...</p>}
         {status === 'failed' && <p>Error: {error.message}</p>}
-        <div className="flex gap-6 justify-center flex-wrap container m-auto">
+
+        <div className="flex gap-20 justify-center flex-wrap container m-auto">
           {projects && projects.slice(1, 13).map((project) => (
             <ProjectCard
               coverImage={project.coverImageURL}
