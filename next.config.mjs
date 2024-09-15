47214de2cdd0ad7026ee029d8e480a9ee2f0d7d7
tanvir35web <islammdtanvirul@gmail.com>
2024-09-15 18:34:17 +0600

    image config added on next config

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,11 @@ const nextConfig = {
       remotePatterns: [
         {
           protocol: 'https',
+          hostname: 'i.ibb.co.com',
+          port: '',
+        },
+        {
+          protocol: 'https',
           hostname: 'i.ibb.co',
           port: '',
         },
